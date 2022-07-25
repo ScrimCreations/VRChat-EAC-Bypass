13b01a092f20aa4552467e42006c6d4d3533af69
Scope <98496609+ScrimCreations@users.noreply.github.com>
2022-07-25 19:19:55 -0400

    Add project files.

---
+++ VRChat-EAC-Bypass/VRChat-EAC-Bypass.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <windows.h>
+#include <shellapi.h>
+
+int main()
+{
+    SetConsoleTitleA("Real VRChat EAC Bypass By Scrim");
+    std::cout << "Loading...\n";
+    Sleep(1500);
+    std::cout << "Getting EAC and shitting on it...\n";
+    Sleep(1500);
+    std::cout << "Bypassing...\n";
+    Sleep(900);
+    std::cout << "Dn lol\n";
+    ShellExecute(0, 0, L"https://www.youtube.com/watch?v=O91DT1pR1ew", 0, 0, SW_SHOW);
+}
